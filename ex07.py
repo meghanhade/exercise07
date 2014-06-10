b18522f8a0cd726b5d344d46da125c2eb368e7fc
Hackbright Student <info@hackbrightacademy.com>
2014-06-09 18:13:32 -0700

    Completed ex07

--- ex07.py
+++ ex07.py
@@ -7,19 +7,37 @@ def main():
     open_file = open(in_file) 
 
     all_text = open_file.read()
-
-    words = all_text.split(' ')
-
+    all_text = all_text.lower()
+    without_newlines = all_text.replace('\n',' ')
+    without_newlines = without_newlines.rstrip()
+    words = without_newlines.split(' ')
+   
     word_count = {}
 
     for each in words:
-        each
+
+        each = each.strip(".?,:;!-")
+
         if each in word_count:
             word_count[each] += 1
         else:
             word_count[each] = 1
 
-    print word_count['cats']
+    frequency = {}
+    for k, v in word_count.items():
+        
+        if v in frequency:
+            frequency[v].append(k)
+        else:
+            frequency[v] = [k]
+
+    f_view = frequency.keys()
+    f_view.sort(reverse=True)
+    for key in f_view:
+        print key, sorted(frequency[key])
+
+
+
 
 if __name__ == "__main__":
     main()
